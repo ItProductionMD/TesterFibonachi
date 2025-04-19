2fa2699529577298f505438c65edc954100273b5
butorinserghei@gmail.com <butorinserghei@gmail.com>
2025-04-19 22:07:54 +0300

    Add project files.

---
+++ TesterFibonachi/FibonacciCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TesterFibonacci;
+
+public class FibonacciCalculator
+{
+    public int Input { get; }
+    public int Cycles { get; }
+
+    public int LastResult { get; private set; }
+    public int Total { get; private set; }
+    public long ElapsedMilliseconds { get; private set; }
+
+    public FibonacciCalculator(int input, int cycles)
+    {
+        Input = input;
+        Cycles = cycles;
+    }
+
+    public void Run()
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        Total = 0;
+        for (int i = 0; i < Cycles; ++i)
+        {
+            LastResult = Fibo(Input);
+            Total += LastResult;
+        }
+
+        stopwatch.Stop();
+        ElapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+    }
+
+    private int Fibo(int v)
+    {
+        if (v < 2)
+            return 1;
+
+        return Fibo(v - 1) + Fibo(v - 2);
+    }
+
+    public void PrintReport()
+    {
+        Console.Write($"\n{Input}-th Fibonacci number is {LastResult}\n" +
+        $"cycles {Total}\n" +
+        $"time elapsed with OOP:{ElapsedMilliseconds} ms\n");
+    }
+}
+
---
+++ TesterFibonachi/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using TesterFibonacci;
+
+Console.WriteLine("Fibonacci Test");
+while (true)
+{
+    Console.WriteLine("Input number:");
+    var numberString = Console.ReadLine();
+    Console.WriteLine("Input cycle");
+    var cycleString = Console.ReadLine();
+    int number = int.Parse(numberString);
+    int cycles = int.Parse(cycleString);
+    int total = 0;
+    int f = 0;
+    //OOP style
+    var calculator = new FibonacciCalculator(number, cycles);
+    calculator.Run();
+    calculator.PrintReport();
+    //procedure style
+    var stopWatch = new Stopwatch();
+    stopWatch.Start();
+    for (int i = 0; i < cycles; ++i)
+    {
+        f = Fibo(number);
+        total += f;
+    }
+    stopWatch.Stop();
+    Console.Write($"\n{number}-th Fibonacci number is {f}\n" +
+        $"cycles {total}\n" +
+        $"time elapsed with function:{stopWatch.ElapsedMilliseconds} ms\n");
+
+    Console.WriteLine("\nPress Enter to Continue or 'exit' to end the program.");
+    if (Console.ReadLine()?.Trim().ToLower() == "exit")
+        break;
+
+}
+
+
+static bool Less(int left, int right)
+{
+    return left < right;
+}
+
+static int Sub(int left, int right)
+{
+    return left - right;
+}
+
+static int Add(int left, int right)
+{
+    return left + right;
+}
+
+static int Fibo(int v)
+{
+    if (Less(v, 2))
+    {
+        return 1;
+    }
+    return Add(Fibo(Sub(v, 1)), Fibo(Sub(v, 2)));
+}
+
